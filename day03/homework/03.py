afbf1f82740f12f82fe2b698833099af1b89ea95
mari13328 <maribol1301@gmail.com>
2025-07-11 13:58:08 +0400

    1

---
+++ day03/homework/03.py
@@ -0,0 +1,76 @@
+1.
+# a1 = 50                                 
+
+# a2 = 80 
+
+# a3 = a1 + a2
+
+# print(a3)
+
+# b1 = 30
+
+# b2 = 10 
+
+# b3 = b1 - b2
+
+# print(b3)
+ 
+2.
+# name1 =("teolina")
+# name1 =("teo")
+# name1 = ("lina")
+
+# print = (name1)
+
+
+
+
+
+3. 
+# name = ("mari")
+# Name = ("mari")
+# nAme = ("mari")
+# namE = ("mari")
+
+
+# print(name)
+# print(Name)
+# print(nAme)
+# print(namE)
+
+
+
+
+
+
+
+4.
+# 2name = "georgia"                           name = "georgia"
+# user{name = "bubunauri"                     user_name = "bubunauri"
+# user_name = goga                            user_name = "goga"
+# user-surname = axalaia                      user_surname = "axalaia"
+
+
+
+5.
+# cipri = 10
+# cIpri = 15
+# ciPri = 20
+# cipRi = 25
+# ciprI = 30
+
+# print(cipri)
+# print(cIpri)
+# print(ciPri)
+# print(cipRi)
+# print(ciprI)
+
+
+
+
+
+6.
+# name = "nana"
+# number = 10 
+# print(name * number)
+
