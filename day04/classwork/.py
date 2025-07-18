05b2588ad4614581b674b63085038ddb4bede81d
mari13328 <maribol1301@gmail.com>
2025-07-18 20:07:17 +0400

    1

---
+++ day04/classwork/.py
@@ -0,0 +1,71 @@
+#data types - მონაცემთა ტიპები
+
+#string-სტრინგი არის ნებისმიერი რამე რაც იქნება მოქცეული " " - ბრჭყალებს შორის იქნება ეს ტექსტური მნიშვნელობა თუ 
+# რიცხვი.
+
+#integer-ინტეჯერი არის ნებისმიერი მთლიანი რიცხვი ჩვენს პროგრამაში,ანუ python-ში.
+
+#float-ჩვენს პროგრამაშ და ზოგადად python-shi 
+
+#boolean- true and false
+
+
+
+
+
+
+
+# print("davit")   
+
+# print(5 + "4" ) #error
+
+# print("daviti" + "goa") #daviti Goa
+
+# print("5" + "5" )# 55 but string value
+
+# surname = "shavadze"
+# name = "davit"
+
+# print(name + surname)
+
+#--------------------------------------------------------------------------------------------------------------
+
+# print("5")
+
+# print(5)
+
+# print(type(5)) #type -> integer
+# print(type("davit")) #type ->
+# #print(type(3.14)) #type -> float / class ->
+# print(type(true)) # boolean / class -> 
+
+#--------------------------------------------------------------------------------------------------------
+
+#input and outputs
+
+
+# name = input("Enter your name :") #mari
+
+# print(name) 
+
+# print("hello,your name is "+ name )
+
+# print(type(str(5) ) )
+
+# print(bool(1))
+
+# print(bool(0))
+
+
+#-------------------------------------------------------------------------------------------------------------
+ 
+
+name = input("Enter your name :")
+
+print("hello, your name is "+ name)
+
+#print(13))#type -> ineger
+#print(type(true)) #boolean
+
+
+
