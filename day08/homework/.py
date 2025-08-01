d7de00351866dc278118cbd96e1a9243d55d452f
mari13328 <maribol1301@gmail.com>
2025-08-01 13:48:28 +0400

    1

---
+++ day08/homework/.py
@@ -0,0 +1,63 @@
+# 1)
+print(5 > 3 and 2 < 4) #true
+print(5 == 5 and 3 < 10) #true
+print(22 == 22 and 13 > 3) #true
+print( 10 > 5 and 11 < 20) #true
+print(11 == 11 and 20 == 20) #true
+
+print(10 > 11 and 33 < 5) #false
+print(11 == 22 and 30 == 13) #false
+print(3 > 40 and 40 < 3) #false
+print(12 > 20 and 10 == 20) #false
+print(13 == 10 and 22 == 7) #false
+
+
+#2)
+# sequencing -  არის თანმიმდევრულად
+# selection -არის არჩევანი
+# iteration - არის განმეორება
+
+#3)
+name = "mari"
+height = 1,68
+print(name)
+print(height)
+# ეს არის sequencing-ის მაგალითი, რადგან კოდი სრყლდება თანმიმდებრობით
+
+#8)
+color = "purple"
+for i in range(46):
+    print(color)
+
+
+
+
+
+#7)
+surname = "bolkvadze"
+for i in range(100):
+    print(surname)
+
+
+
+#9)
+name = "mari"
+for i in range(32):
+    print(name)
+
+
+
+#12)
+n1 = int(input("რიცხვი 1:"))
+n2 = int(input("რიცხვი 2 :"))
+n3 = int(input("რიცხვი 3 :"))
+n4 = int(input("რიცხვი 4 :"))
+
+sum = n1 + n2 + n3 + n4
+print(sum)  
+
+
+
+
+
+
