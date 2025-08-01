397f6d7b28c95815748a0ec56a35c3af5df3bad9
mari13328 <maribol1301@gmail.com>
2025-08-01 14:54:57 +0400

    1

--- day05/homework/.py
+++ day05/homework/.py
@@ -1,3 +1,57 @@
 print(15 > 5 and 5 < 15)
 print(60 > 3 and 3 < 60)
 
+
+
+# 7)
+
+a = "40"
+b = "20"
+c = "10"
+d = "30"
+
+sum =(a + b + c + d)
+print(sum)
+
+
+#8)
+
+n1 = 30
+n2 = 40
+n3 = 50
+n4 = n1 + n2
+print(n3)
+
+# 6)
+
+str1 = "hello"
+star1="helloo"
+print(star1)
+
+
+
+# 5)
+
+x = 42  #int
+y = "mari" #str
+z = 3.4 #float
+is_ready = True #bool
+
+
+# 3)
+
+n1 = input("შეიყვანე სტრინგ 1: ")
+n2 = input("შეიყვანე სტრინგ 2: ")
+n3 = input("შეიყვანე სტრინგ 3: ")
+n4 = input("შიყვანე სტრინგ 4: ")
+n5 = input("შიყვანე სტრინგ 5: ")
+
+
+#4)
+
+b1 =float(input("შეიყვანე პირველი რიცხვი: "))
+b2 = float(input("შეიყვანე მეორე რიცხვი: "))
+b3 =float(input("შეიყვანე მესამე რიცხვი: "))
+b4 =float(input("შიყვანე მეოთხე რიცხვი: "))
+
+
