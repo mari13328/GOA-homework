6a14b9a13bbea015b93b863408677837e0cd78c5
mari13328 <maribol1301@gmail.com>
2025-08-27 00:32:06 +0400

    1

---
+++ day15/homewrok/.py
@@ -0,0 +1,37 @@
+age = 12
+if age > 10:
+    print("more then 10")
+else:
+    print("less then 10")
+
+
+number = int(input("enter a number: "))
+if number == 15:
+    print("equal to 15")
+else:
+    print("not eaqual to 15")
+
+
+
+name = input("enter your name")
+if name == ("giorgi"):
+    print("you are correct")
+else:
+    print("you are wrong")
+
+
+
+for i in range(50,101,5):
+    print(i)
+
+
+
+for i in range(5):
+    print("mari bplkvadze")
+
+
+
+i = 20
+while i <50:
+    print(i)
+i = i + i
