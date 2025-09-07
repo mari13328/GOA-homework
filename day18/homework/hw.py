8ce0b9838818084cfdb8d7048140e2d2de95a81b
mari13328 <maribol1301@gmail.com>
2025-09-07 17:46:03 +0400

    1

---
+++ day18/homework/hw.py
@@ -0,0 +1,50 @@
+num1=input("enter your name")
+
+name = "mari"
+
+if num1 == name:
+    print("სეხნიები ვართ")
+else:
+    print("სხვადასხვა სახელები გვაქვს")
+
+
+num2 = int(input("enter first number"))
+
+num3 = int(input("enter second name"))
+
+
+if num2 > num3:
+    print("second is more then third")
+
+elif num2 < num3:
+    print("second is lees then third")
+
+else:
+    print("second number equal to third number")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
