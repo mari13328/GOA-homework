844c06b803b2351fe099a21aee1d499e00b166dd
mari13328 <maribol1301@gmail.com>
2025-09-16 21:08:09 +0400

    1

---
+++ day22/classwork/.py
@@ -0,0 +1,71 @@
+# box = ["goga","nuca","mari","ele","nana",]
+
+# print(box[2])
+# print(box[4])
+
+#------------------------------------------------------------------------------------------------
+
+# box2 = ["nana","mari","achiko","temuri","anastaso","mzia","qeti","liza","davit","kaxa"]
+
+# print(box2[0])
+# print(box2[1])
+# print(box2[2])
+# print(box2[3])
+# print(box2[4])
+# print(box2[5])
+# print(box2[6])
+# print(box2[7])
+# print(box2[8])
+# print(box2[9])
+
+# box = ["goga","nuca","mari","ele","nana",]
+
+
+# box[1] = "sesili"   
+
+# print(box[1])
+
+
+
+vegetabels = ["კარტოპილი","სტაფილო","კიტრი","პამიდორი","ხახვი","ბროკოლი"]
+
+
+vegetabels[0] = "viri"
+vegetabels[3] = "tagvit"
+vegetabels[5] = "bokveri"
+
+print(vegetabels[0])
+print(vegetabels[1])
+print(vegetabels[2])
+print(vegetabels[3])
+print(vegetabels[4])
+print(vegetabels[5])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
