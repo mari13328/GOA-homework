afa9e09ef5284008ba372f8dce0ded528aff3881
mari13328 <maribol1301@gmail.com>
2025-09-27 23:17:47 +0400

    1

--- day22/homework/hw.py
+++ day22/homework/hw.py
@@ -1,1 +1,24 @@
 
+numbers = [4,6,1,5,9,8,4]
+
+num  = numbers[0] + numbers[1]
+
+num1 = numbers[2] + numbers[2]
+
+num2 = numbers[1] + numbers[5]
+
+num3 = numbers[3] + numbers[4]
+
+num4 = numbers[2] + numbers[4]
+
+num5 = numbers[0] + numbers[2]
+
+num6 = numbers[4] + numbers[5]
+
+
+
+
+
+
+
+
---
+++ day24/classwork/hb.py
@@ -0,0 +1,51 @@
+
+
+
+names = ["nana","ele","anastaso","nuca","ekato","mzia","liza","qeta"]
+
+print(names[-1])
+print(names[-2])
+print(names[-3])
+print(names[-4])
+print(names[-5])
+print(names[-6])
+print(names[-7])
+print(names[-8])
+
+
+
+name1 = "liziko"
+
+name2 = "qetato"
+
+name3 = "mari"
+
+name4 = "ana"
+
+name5= "seso"
+
+
+name =[name1,name2,name3,name4,name5]
+
+print(name[-1])
+
+
+
+
+
+
+
+name7 ="irakli"
+
+print(name7[3])
+
+print(name7[-3])
+
+
+
+
+
+
+
+
+
