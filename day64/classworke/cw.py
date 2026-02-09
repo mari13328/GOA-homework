602c03a6f5a8b5f4a1e02a6271ade63628fb3fc1
mari13328 <maribol1301@gmail.com>
2026-02-09 22:19:17 +0400

    1

---
+++ day64/classworke/cw.py
@@ -0,0 +1,73 @@
+
+# def --> define == sheqmna
+
+def greet():
+
+    return "hello my friend"
+
+print(greet())
+
+
+                                                                                                                        
+
+
+def say_my_name():
+
+    print("my name is Mari")
+
+say_my_name()
+say_my_name()
+
+
+
+
+def check_even():
+
+    number = 7
+    if number % 2 == 0:
+        return "this number is even"
+    else:
+        return "this number is odd"
+
+
+print(check_even())
+
+
+
+                                                                                                                        
+
+def check_number(number):
+
+    if number > 0:
+        return "more than 0"
+    elif number < 0 :
+        return "lass than 0"
+    else:
+        return "equal to 0"
+
+print(check_number(18))
+print(check_number(0))
+print(check_number(-13))
+
+
+
+                                                                                                                        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
