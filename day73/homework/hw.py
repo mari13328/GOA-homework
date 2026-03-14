f2993170be77501812811d0855d17112d51221bb
mari13328 <maribol1301@gmail.com>
2026-03-14 23:02:02 +0400

    1

---
+++ day73/homework/hw.py
@@ -0,0 +1,13 @@
+
+def stringly(size):
+    caamate = ""
+
+    for i in range(size):
+        if i % 2 == 0:
+            caamate += "1"
+        else:
+            caamate += "0"
+
+        return caamate
+
+
