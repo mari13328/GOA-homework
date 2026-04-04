e1a901f6512cab22ca5f0aaacc5270bf2b8f7127
mari13328 <maribol1301@gmail.com>
2026-04-04 20:07:03 +0400

    1

--- day76/homeworke/hw.py
+++ day76/homeworke/hw.py
@@ -6,6 +6,7 @@ def number(bus):
     pp1 = 0
     pp2 = 0
 
+
     for el in range (len(bus)):
         pp1 += bus[el][0]
         pp2 +=  [el][1]
---
+++ day79/homeworke/hw.py
@@ -0,0 +1,8 @@
+
+def reverse_list(L):
+    return list[::1]
+
+#or
+
+def reverse_list(L):
+    return list(reversed(L))
